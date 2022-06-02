dc404856f6fb4025bc3e04b400b76c71bf828481
Miah Clay <miahb88@gmail.com>
2022-06-02 16:15:51 -0400

    working on it

--- boggle_board.py
+++ boggle_board.py
@@ -1,6 +1,22 @@
+import string
+import random
+
 class BoggleBoard:
   
-  def __init__(self):
-  
+  def __init__(self, board=[[]]*4):
+    self.board = board
 
   def shake(self):
+    abc = string.ascii_uppercase.replace('Q','Qu')
+    print(abc)
+    for line in self.board:
+      for j in range(len(self.board)):
+        rand = random.choice(abc)
+        line.append(rand)
+      print(self.board)
+
+
+
+game = BoggleBoard()
+print(game.board)
+game.shake()
